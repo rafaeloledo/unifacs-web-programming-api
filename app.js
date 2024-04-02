1fdd08e68ff4843c163f2fd34219da6cb01ab17f
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-04-01 23:03:56 -0300

    chore(js): update app
    add support to body-parser and rendering view

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const productsRouter = require('./src/products')
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.use(express.json())
 app.use('/products', productsRouter)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/view/index.html")
+})
 
 const PORT = process.env.PORT || 3000
 
